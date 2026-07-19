ca8976375767b61e47b442ce13c21aef1bd61465
Aspiya-A <Aspiya.A@kodnext.in>
2026-07-19 15:27:09 +0530

    changes

--- ContrackAPI/Models/Container/ContainerFilterPage.cs
+++ ContrackAPI/Models/Container/ContainerFilterPage.cs
@@ -19,10 +19,10 @@ namespace ContrackAPI
         public int GetFilterCount()
         {
             int count = 0;
-            if (!string.IsNullOrEmpty(filters.containertype_encry)) count++;
-            if (!string.IsNullOrEmpty(filters.containersize_encry)) count++;
-            if (!string.IsNullOrEmpty(filters.containermodel_encry)) count++;
-            if (!string.IsNullOrEmpty(filters.location_encry)) count++;
+            //if (!string.IsNullOrEmpty(filters.containertype_encry)) count++;
+            //if (!string.IsNullOrEmpty(filters.containersize_encry)) count++;
+            //if (!string.IsNullOrEmpty(filters.containermodel_encry)) count++;
+            //if (!string.IsNullOrEmpty(filters.location_encry)) count++;
             if (!string.IsNullOrEmpty(filters.pol_encry)) count++;
             if (!string.IsNullOrEmpty(filters.pod_encry)) count++;
             if (!string.IsNullOrEmpty(filters.voyage_encry)) count++;
@@ -35,38 +35,52 @@ namespace ContrackAPI
     }
     public class ContainerFilter
     {
-        public string containertype_encry { get; set; }
-        public string containersize_encry { get; set; }
-        public string containermodel_encry { get; set; }
-        public string location_encry { get; set; }
+        public List<string> containertypeencids { get; set; } = new();
+        public List<string> containersizeencids { get; set; } = new();
+
+        public List<string> containermodeluuids { get; set; } = new();
+        public List<string> locationuuids { get; set; } = new();
+
         public string pol_encry { get; set; }
         public string pod_encry { get; set; }
         public string voyage_encry { get; set; }
-        public string voyage_name { get; set; }
         public string move_encry { get; set; }
+        public string operator_encry { get; set; }
+
+        public string bookingno { get; set; }
+
         [JsonProperty("status_selected")]
         public int status { get; set; }
-        public List<long> containertypeids { get; set; } = new List<long>();
-        public List<int> containersizeids { get; set; } = new List<int>();
-        public List<string> containermodeluuids { get; set; } = new List<string>();
-        //public List<string> locationdetailids { get; set; } = new List<string>();
-        public List<int> pols { get; set; } = new List<int>();
-        public List<int> pods { get; set; } = new List<int>();
-        public List<int> voyageids { get; set; } = new List<int>();
-        public List<int> moveids { get; set; } = new List<int>();
-        [JsonProperty("status")]
-        public List<int> status_list { get; set; } = new List<int>();
+
         [JsonProperty("operator_selected")]
         public int operatortype { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore]
 
-        [JsonProperty("operator_list")]
-        public List<int> operator_list { get; set; } = new List<int>();
+        public int active_operator { get; set; }
 
-        public string operator_encry { get; set; }
-        public List<int> operatorids { get; set; } = new List<int>();
-        public int active_operator { get; set; } = 0;
-        public List<string> locationuuids { get; set; } = new List<string>();
-        public string bookingno { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore]
+        public List<long> containertypeids { get; set; } = new();
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public List<int> containersizeids { get; set; } = new();
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public List<int> pols { get; set; } = new();
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public List<int> pods { get; set; } = new();
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public List<int> voyageids { get; set; } = new();
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public List<int> moveids { get; set; } = new();
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public List<int> operatorids { get; set; } = new();
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public List<int> status_list { get; set; } = new();
     }
     public class ContainerStatusCountDTO
     {
